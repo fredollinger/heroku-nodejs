a53d02b785777dff34a1abe29b6961764d1a34a8
Frederick Ollinger <fred.ollinger@seescan.com>
2014-09-30 16:16:27 -0700

    Added two tests.

--- js/acvalidater.js
+++ js/acvalidater.js
@@ -2,7 +2,9 @@ function ACValidator() {
     console.log('acvalid init');
     // this.type = type;
     // this.color = "red";
-    this.validatePhoneNumber = function (number){
-        console.log('validate phone number');
+    this.validatePhoneNumber = function (p){
+        var phoneRe = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
+        var digits = p.replace(/\D/g, "");
+        return (digits.match(phoneRe) !== null);
     }
 } // END ACValidator
--- test/validate.js
+++ test/validate.js
@@ -5,6 +5,19 @@ load("js/acvalidater.js");
 
 vali = new ACValidator();
 number="619-867-5309"
-//console.log(getMethods(vali));
-vali.validatePhoneNumber(number);
-//vali.bob();
+
+if (vali.validatePhoneNumber(number)){
+    console.log("phone number test 1 passed");
+}
+else{
+    console.log("failed to validate phone number");
+}
+
+number="1619-867-5309"
+
+if (!vali.validatePhoneNumber(number)){
+    console.log("phone number test 2 passed");
+}
+else{
+    console.log("failed to invalidate a purposefully invalid phone number");
+}
