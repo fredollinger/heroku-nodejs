2c91cd85ad681782e0471880e34690d699062bc0
Fred Ollinger <follinge@gmail.com>
2014-11-30 16:24:21 -0800

    Now we load map properly!

--- public/core.js
+++ public/core.js
@@ -1,22 +1,8 @@
-function loadScript(url, callback){
-
-    var script = document.createElement("script")
-    script.type = "text/javascript";
-
-    if (script.readyState){  //IE
-        script.onreadystatechange = function(){
-            if (script.readyState == "loaded" ||
-                    script.readyState == "complete"){
-                script.onreadystatechange = null;
-                callback();
-            }
-        };
-    } else {  //Others
-        script.onload = function(){
-            callback();
-        };
+function loadScript(thescript) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = thescript;
+    script.onload = function(){
+        document.body.appendChild(script);
     }
-
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
 }
